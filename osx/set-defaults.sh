7cc3fe8cc2863b7a23cc9e79dc02e353c08eef2f
freek <freek@spatie.be>
2014-05-09 09:14:56 +0200

    commit

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -172,10 +172,10 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+#defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults write com.apple.screencapture type -string "png"
+#defaults write com.apple.screencapture type -string "png"
 
 # Disable shadow in screenshots
 #defaults write com.apple.screencapture disable-shadow -bool true
