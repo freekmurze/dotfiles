774be903002269d68067ee750d182b3bd7d6f30f
Mert Aşan <mertemreasan@gmail.com>
2021-06-22 17:36:25 +0300

    Update set-defaults.sh

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -170,6 +170,19 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
 	Privileges -bool true
 
 ###############################################################################
+# Screenshots                                                                 #
+###############################################################################
+
+# Set default screenshot location
+defaults write com.apple.screencapture "location" -string "~/Documents/Screenshots"
+
+# Exclude date and time in screenshot filenames
+defaults write com.apple.screencapture "include-date" -bool false
+
+# Change the default screenshot file name
+defaults write com.apple.screencapture "name" -string "screenshot"
+
+###############################################################################
 # Dock, Dashboard, and hot corners                                            #
 ###############################################################################
 
